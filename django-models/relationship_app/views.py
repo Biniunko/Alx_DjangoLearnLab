a3a988a26752eb475dc4293c33d2c9491a3d9d32
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-11-15 23:35:41 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Function-based view to list all books
 def list_books(request):
