d79c863ee4a8104ff1bede80e362dab9f69bd177
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-11-15 22:44:58 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -16,7 +16,7 @@ def get_books_in_library(library_name):
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name).first()
+    library = Librarian.objects.get(name="").first()
     if library:
         return library.librarian
     return None
