7280cd89d54b15e3432086282f62387caef7a6d6
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-11-17 17:02:44 +0300

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -9,3 +9,24 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('publication_year',)
     search_fields = ('title', 'author')
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+# Custom User Admin
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom User Admin to manage CustomUser in Django admin.
+    """
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email', 'date_of_birth', 'profile_photo')}),
+    )
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
+    search_fields = ('email', 'username')
+
+# Register the CustomUser model with the CustomUserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
