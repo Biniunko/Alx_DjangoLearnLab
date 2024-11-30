d4346ace49d482def5b53b74e1fdc286dbe46666
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-12-01 01:10:06 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,7 @@ from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
+from django_filters import rest_framework
 class BookListView(generics.ListAPIView):
     """
     Retrieves a list of all books.
