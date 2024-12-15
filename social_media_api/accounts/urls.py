b4655ac4b95754fcbb731c635002b5944b58efe1
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-12-15 22:15:40 +0300

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,6 +2,12 @@ from django.urls import path, include
 from .views import RegisterView, LoginView
 from .views import UserViewSet
 from rest_framework.routers import DefaultRouter
+from .views import FollowUserView, UnfollowUserView
+
+urlpatterns = [
+    path('follow/<str:username>/', FollowUserView.as_view(), name='follow_user'),
+    path('unfollow/<str:username>/', UnfollowUserView.as_view(), name='unfollow_user'),
+]
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
