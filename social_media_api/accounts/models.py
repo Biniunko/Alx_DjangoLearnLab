a1770b3e7e9c992795b3d7c2ac25582fa07d6568
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-12-15 23:19:09 +0300

    added social_media_api

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(
+        upload_to="profile_pics/", null=True, blank=True
+    )
+    followers = models.ManyToManyField("self", blank=True, symmetrical=False)
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from .serializers import RegisterSerializer
+from django.contrib.auth import authenticate
+
+
+@api_view(["POST"])
+def register(request):
+    if request.method == "POST":
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User created successfully"}, status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    user = authenticate(username=username, password=password)
+    if user:
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
+    return Response(
+        {"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
+    )
