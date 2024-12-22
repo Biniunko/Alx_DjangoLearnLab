38b19947b08b5837aa0fd55f589762b690b242df
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-12-22 21:11:07 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -34,17 +34,18 @@ def login(request):
         {"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
     )
 
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
-@api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated])
-def follow_user(request, user_id):
-    user_to_follow = User.objects.get(id=user_id)
-    request.user.following.add(user_to_follow)
-    return Response({'message': f'Now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
+    @action(detail=True, methods=["post"], url_path="follow")
+    def follow_user(self, request, pk=None):
+        user_to_follow = self.get_object()
+        request.user.following.add(user_to_follow)
+        return Response({"status": "following"}, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated])
-def unfollow_user(request, user_id):
-    user_to_unfollow = User.objects.get(id=user_id)
-    request.user.following.remove(user_to_unfollow)
-    return Response({'message': f'Unfollowed {user_to_unfollow.username}'}, status=status.HTTP_200_OK)
+    @action(detail=True, methods=["post"], url_path="unfollow")
+    def unfollow_user(self, request, pk=None):
+        user_to_unfollow = self.get_object()
+        request.user.following.remove(user_to_unfollow)
+        return Response({"status": "unfollowing"}, status=status.HTTP_200_OK)
