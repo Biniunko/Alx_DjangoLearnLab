3e7c59d6045de2173b53785a789c99e8734b24a3
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-12-22 21:20:56 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import status, permissions, viewsets
+from rest_framework.decorators import api_view, action, permission_classes
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth import authenticate
-from rest_framework import status, permissions
-from rest_framework.decorators import permission_classes
-from .models import User
-from .serializers import UserSerializer
+from .models import CustomUser
+from rest_framework import generics  # Importing GenericAPIView
 
 @api_view(["POST"])
 def register(request):
@@ -34,7 +32,7 @@ def login(request):
         {"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
     )
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserViewSet(generics.GenericAPIView, viewsets.ReadOnlyModelViewSet):  # Adding GenericAPIView
     queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
